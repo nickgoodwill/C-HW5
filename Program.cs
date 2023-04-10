c0bf1c28740e35b8f30699ba0bd2b68906c4126d
Nick Goodwill <nick.goodwill@yandex.ru>
2023-04-10 09:42:46 +0300

    first task

--- Program.cs
+++ Program.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+
+int [] CreateArray(int Length)
+{
+int [] numbers = new int [Length];
+
+for (int i = 0; i < numbers.Length; i++)
+{
+numbers[i] = new Random().Next(100,1000);
+}
+return numbers;
+}
+
+void PrintArray(int [] numbers)
+{
+System.Console.Write("[");
+for (int i = 0; i < numbers.Length -1; i++)
+{
+System.Console.Write(numbers[i] + "; ");
+}
+System.Console.WriteLine(numbers[numbers.Length -1] + "]");
+}
+
+int Num (string arg)
+{
+System.Console.WriteLine($"Введите {arg} ");
+return int.Parse(System.Console.ReadLine());
+}
+
+int EvenQty (int [] numbers)
+{
+int qty = 0;
+for (int i = 0; i < numbers.Length; i++)
+{
+if(numbers[i] % 2 == 0)
+{
+    qty++;
+}
+}
+return qty;
+}
+
+int Length = Num("длину массива");
+int [] numbers = CreateArray(Length);
+PrintArray(numbers);
+System.Console.WriteLine(EvenQty(numbers));
