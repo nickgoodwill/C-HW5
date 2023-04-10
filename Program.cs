06420152c77248bc0fe53feacf9d79cd0f0fc37b
Nick Goodwill <nick.goodwill@yandex.ru>
2023-04-10 10:12:46 +0300

    second task

--- Program.cs
+++ Program.cs
@@ -1,13 +1,61 @@
 ﻿// Задайте массив заполненный случайными положительными трёхзначными числами. 
 // Напишите программу, которая покажет количество чётных чисел в массиве.
 
+// int [] CreateArray(int Length)
+// {
+// int [] numbers = new int [Length];
+
+// for (int i = 0; i < numbers.Length; i++)
+// {
+// numbers[i] = new Random().Next(100,1000);
+// }
+// return numbers;
+// }
+
+// void PrintArray(int [] numbers)
+// {
+// System.Console.Write("[");
+// for (int i = 0; i < numbers.Length -1; i++)
+// {
+// System.Console.Write(numbers[i] + "; ");
+// }
+// System.Console.WriteLine(numbers[numbers.Length -1] + "]");
+// }
+
+// int Num (string arg)
+// {
+// System.Console.WriteLine($"Введите {arg} ");
+// return int.Parse(System.Console.ReadLine());
+// }
+
+// int EvenQty (int [] numbers)
+// {
+// int qty = 0;
+// for (int i = 0; i < numbers.Length; i++)
+// {
+// if(numbers[i] % 2 == 0)
+// {
+//     qty++;
+// }
+// }
+// return qty;
+// }
+
+// int Length = Num("длину массива");
+// int [] numbers = CreateArray(Length);
+// PrintArray(numbers);
+// System.Console.WriteLine(EvenQty(numbers));
+
+// Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+
 int [] CreateArray(int Length)
 {
 int [] numbers = new int [Length];
 
 for (int i = 0; i < numbers.Length; i++)
 {
-numbers[i] = new Random().Next(100,1000);
+numbers[i] = new Random().Next(1,10);
 }
 return numbers;
 }
@@ -28,20 +76,17 @@ System.Console.WriteLine($"Введите {arg} ");
 return int.Parse(System.Console.ReadLine());
 }
 
-int EvenQty (int [] numbers)
+int Sum (int [] numbers)
 {
-int qty = 0;
-for (int i = 0; i < numbers.Length; i++)
+int sum = 0;
+for (int i = 1; i < numbers.Length; i=i+2)
 {
-if(numbers[i] % 2 == 0)
-{
-    qty++;
+    sum = sum + numbers[i];
 }
-}
-return qty;
+return sum;
 }
 
 int Length = Num("длину массива");
 int [] numbers = CreateArray(Length);
 PrintArray(numbers);
-System.Console.WriteLine(EvenQty(numbers));
+System.Console.WriteLine(Sum(numbers));
